af241ed688b31db1d4a13064b3a801bba90a682f
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2023-08-28 22:43:46 +0900

    feat: sidebar scrollspy 기능 구현

--- libs/mdx.ts
+++ libs/mdx.ts
@@ -1,14 +1,14 @@
 import { serialize } from "next-mdx-remote/serialize";
+import rehypeAutolinkHeadings from "rehype-autolink-headings/lib";
 import rehypePrism from "rehype-prism";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
-import remarkToc from "remark-toc";
 
 export const serializeMdx = (source: string) => {
   return serialize(source, {
     mdxOptions: {
       remarkPlugins: [remarkGfm],
-      rehypePlugins: [rehypeSlug, rehypePrism],
+      rehypePlugins: [rehypeSlug, rehypePrism, rehypeAutolinkHeadings],
       format: "mdx",
     },
   });
--- pages/components/SideBar.tsx
+++ pages/components/SideBar.tsx
@@ -1,18 +1,95 @@
 import { TableOfContents } from "@/libs/types";
+import { Fragment } from "react";
+import { useScroll } from "@/src/hooks/useScroll";
+import { Section, SubSection } from "@/libs/types";
+import { $ } from "@/libs/clsx";
 
 export default function Sidebar({
   tableOfContents,
+  className,
 }: {
   tableOfContents: TableOfContents;
+  className?: string;
 }): React.ReactElement {
-  const tocArray = tableOfContents.map((toc) => toc.text);
+  const { currentSectionSlug } = useScroll(tableOfContents);
+
+  const isSubSectionActive = (subSection: SubSection) => {
+    return subSection.slug === currentSectionSlug;
+  };
+
+  const isSectionActive = (section: Section) => {
+    return (
+      section.slug === currentSectionSlug ||
+      section.subSections.some((v) => v.slug === currentSectionSlug)
+    );
+  };
+
   return (
-    <div className="sticky border-2 border-gray h-full z-500 p-4 top-40 ml-14 rounded-xl">
-      {tocArray.map((toc, index) => (
-        <p key={index} className="p-1">
-          {toc}
-        </p>
-      ))}
+    <div
+      className={$(
+        "overflow-hidden h-full sticky top-80 ml-12 rounded-xl border border-neutral-200 transition-all dark:border-neutral-800",
+        className
+      )}
+    >
+      {tableOfContents.length !== 0 && (
+        <div className="p-4 pr-2 dark:border-neutral-700 dark:bg-neutral-800">
+          <ul
+            id="toc-content"
+            className="mt-2 flex flex-col items-start justify-start text-sm"
+          >
+            {tableOfContents.map((section) => (
+              <Fragment key={section.slug}>
+                <li>
+                  <a
+                    href={`#${section.slug}`}
+                    className={$(
+                      "group block py-1",
+                      section.subSections && "",
+                      isSectionActive(section)
+                        ? "text-blue"
+                        : "text-white dark:text-black"
+                    )}
+                  >
+                    {section.text}
+                  </a>
+                </li>
+                {section.subSections.map((subSection) => (
+                  <li key={subSection.slug} className="ml-4">
+                    <a
+                      href={`#${subSection.slug}`}
+                      className={$(
+                        "group flex items-start py-1",
+                        isSubSectionActive(subSection)
+                          ? "text-blue"
+                          : "text-white dark:text-black"
+                      )}
+                    >
+                      <svg
+                        width="3"
+                        height="24"
+                        viewBox="0 -9 3 24"
+                        className={$(
+                          "mr-2 overflow-visible",
+                          "text-tertiary group-hover:text-secondary"
+                        )}
+                      >
+                        <path
+                          d="M0 0L3 3L0 6"
+                          fill="none"
+                          stroke="currentColor"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                        />
+                      </svg>
+                      {subSection.text}
+                    </a>
+                  </li>
+                ))}
+              </Fragment>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
