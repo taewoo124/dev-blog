540c1554662d3c867d308700e9669a3de28a1ddb
taewoo124 <121784425+taewoo124@users.noreply.github.com>
2024-01-07 10:27:32 +0900

    post: es14 post

--- pages/components/Title.tsx
+++ pages/components/Title.tsx
@@ -1,13 +1,12 @@
 import { Post } from "@/libs/types";
 
 export default function Title({ post }: { post: Post }): React.ReactElement {
-  const { title, date, description } = post;
   return (
     <section className="flex justify-center">
       <div className="flex flex-col items-center border-b-2 border-gray mt-14 w-full">
-        <h1 className="text-6xl mb-12">{title && title}</h1>
-        <h2 className="text-2xl mb-12">{description && `"${description}"`}</h2>
-        <p className="mb-4">{date && date}</p>
+        <h1 className="text-6xl mb-12">{post && post.title}</h1>
+        <h2 className="text-2xl mb-12">{post && `"${post.description}"`}</h2>
+        <p className="mb-4">{post && post.date}</p>
       </div>
     </section>
   );
